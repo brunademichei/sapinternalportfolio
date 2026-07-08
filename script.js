f9b787056c008c20058cef69e5fd2b3a503af6da
Devin <devin@example.com>
2026-07-08 19:05:14 +0000

    Add Bad Money / Good Money discovery session case (FigJam board, scenario flow, phased roadmap)

--- script.js
+++ script.js
@@ -87,7 +87,7 @@
           observer.unobserve(entry.target);
         }
       });
-    }, { threshold: 0.12, rootMargin: "0px 0px -8% 0px" });
+    }, { threshold: 0, rootMargin: "0px 0px -8% 0px" });
 
     revealEls.forEach(function (el) { observer.observe(el); });
   }
